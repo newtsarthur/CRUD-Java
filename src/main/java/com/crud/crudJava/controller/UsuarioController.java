f107446415d886379fa1e6d5090a05b84166efa0
newtsarthur <tutuhoficial13@gmail.com>
2025-01-26 14:24:55 -0300

    corrigido bugs no get e no post, CRUD funcionando

--- src/main/java/com/crud/crudJava/controller/UsuarioController.java
+++ src/main/java/com/crud/crudJava/controller/UsuarioController.java
@@ -1,7 +1,10 @@
 package com.crud.crudJava.controller;
 
 import com.crud.crudJava.model.Usuario;
+import com.crud.crudJava.repository.UsuarioRepository;
 import com.crud.crudJava.service.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,21 +13,29 @@ import java.util.List;
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
+
+//    @Autowired
     private final UsuarioService service;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     public UsuarioController(UsuarioService service) {
         this.service = service;
     }
 
-    @GetMapping
-    public List<Usuario> listarTodos() {
-        return service.listarTodos();
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getUserById(@PathVariable Long id) {
+        return service.buscarPorId(id)
+                .map(usuario -> ResponseEntity.ok(usuario))
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public Usuario salvar(@RequestBody Usuario usuario)
-    {
-        return service.salvar(usuario);
+    public ResponseEntity<Usuario> criarUsuario(@RequestBody @Valid Usuario usuario) {
+        Usuario usuarioCriado = service.salvar(usuario);
+        return ResponseEntity.status(201).body(usuarioCriado);
+
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/com/crud/crudJava/service/UsuarioService.java
+++ src/main/java/com/crud/crudJava/service/UsuarioService.java
@@ -2,17 +2,18 @@ package com.crud.crudJava.service;
 
 import com.crud.crudJava.model.Usuario;
 import com.crud.crudJava.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
-    private final UsuarioRepository repository;
 
-    public UsuarioService(UsuarioRepository repository) {
-        this.repository = repository;
-    }
+    @Autowired
+    private UsuarioRepository repository;
 
     public List<Usuario> listarTodos() {
         return repository.findAll();
@@ -20,6 +21,10 @@ public class UsuarioService {
 
     public Usuario salvar(Usuario usuario) {
         return repository.save(usuario);
+    }
+
+    public Optional<Usuario> buscarPorId(Long id) {
+        return repository.findById(id);
     }
 
     public void deletar(Long id) {
